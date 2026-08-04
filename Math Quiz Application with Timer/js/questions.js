51d62c010babd201f137f0629e3d7fb35e334c7b
Mvuselelomtolo48 <mvuselelomtolo48@gmail.com>
2026-08-04 09:04:40 +0200

    Add files via upload

---
+++ Math Quiz Application with Timer/js/questions.js
@@ -0,0 +1,124 @@
+// creating an array and passing number, questions, options, and answers
+const questions = [
+    {
+        numb: 1,
+        question: "(x+5)(x-2) = 0. What are the solutions for x?",
+        answer: "-5 and 2",
+        options: [
+            "-5 and 2",
+            "5 and -2",
+            "5 and 2",
+            "-5 and -2" 
+         ]
+        
+    },
+
+    {
+        numb: 2,
+        question: "5x + 3 = 18. What is the value of x?",
+        answer: "3",
+        options: [
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "10x - 7 = 23. What is the value of x?",
+        answer: "3",
+        options: [
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "2x + 5 = 15. What is the value of x?",
+        answer: "5",
+        options: [
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "x^2 - 4x + 4 = 0. What is the value of x?",
+        answer: "2",
+        options: [
+            "1",
+            "2",
+            "3",
+            "4"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "2x^2 + 3x - 5 = 0. What are the solutions for x?",
+        answer: "1 and -2.5",
+        options: [
+            "1 and -2.5",
+            "1 and 2.5",
+            "-1 and -2.5",
+            "-1 and 2.5"
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "x^2 + 6x + 9 = 0. What is the value of x?",
+        answer: "-3",
+        options: [
+            "-1",
+            "-2",
+            "-3",
+            "-4"
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "number pattern: 2, 4, 8, 16,.... what is the next number ?",
+        answer: "32",
+        options: [
+            "24",
+            "32",
+            "40",
+            "48"
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "number pattern: 1, 1, 2, 3, 5, 8, 13,.... what is the next number ?",
+        answer: "21",
+        options: [
+            "15",
+            "21",
+            "28",
+            "34"
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "solve for x and y: 2x + 3y = 12 and x - y = 1",
+        answer: "x = 3, y = 2",
+        options: [
+            "x = 3, y = 2",
+            "x = 2, y = 3",
+            "x = 4, y = 1",
+            "x = 1, y = 4"  
+        ]   
+    },
+
+]
